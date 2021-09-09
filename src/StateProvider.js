eb765644987d212fea49faaa31468a4fabaa89b5
zhpadvoiski <zh.padvoiski@gmail.com>
2021-09-09 18:16:40 -0500

    prop types

--- src/StateProvider.js
+++ src/StateProvider.js
@@ -1,14 +1,20 @@
-import React, {createContext,useContext,useReducer} from 'react';
+import React, { createContext, useContext, useReducer } from "react";
+import PropTypes from "prop-types";
 
 export const StateContext = createContext();
 //create container where data lives
-export const StateProvider = ({ reducer, initialState, children}) => (
-    //allowed us set the data layer
-    <StateContext.Provider value={useReducer(reducer,initialState)}>
-        {children} {/* App child */}
-    </StateContext.Provider>
+export const StateProvider = ({ reducer, initialState, children }) => (
+  //allowed us set the data layer
+  <StateContext.Provider value={useReducer(reducer, initialState)}>
+    {children} {/* App child */}
+  </StateContext.Provider>
 );
 
 //allowed us pull information from the data layer
 //Check Login.js
+StateProvider.propTypes = {
+  reducer: PropTypes.any,
+  initialState: PropTypes.any,
+  children: PropTypes.any,
+};
 export const useStateValue = () => useContext(StateContext);
