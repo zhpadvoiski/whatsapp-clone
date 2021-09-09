30ab0d9779e4ca34249693963ba0772e3c9f4e30
zhpadvoiski <zh.padvoiski@gmail.com>
2021-09-09 18:18:45 -0500

    props

--- src/components/SidebarChat.js
+++ src/components/SidebarChat.js
@@ -1,54 +1,66 @@
-import React, { useEffect, useState } from 'react';
-import { Link } from 'react-router-dom';
+import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
 import { Avatar } from "@material-ui/core";
-import db from '../firebase';
-import './sidebarChat.css';
+import db from "../firebase";
+import "./sidebarChat.css";
+import PropTypes from "prop-types";
 
-function SidebarChat( {addNewChat, name, id}) {
-    const [seed, setSeed] = useState('');
-    const [messages, setMessages] = useState("");
+function SidebarChat({ addNewChat, name, id }) {
+  const [seed, setSeed] = useState("");
+  const [messages, setMessages] = useState("");
 
-    /* show last message on sideBar */
-    useEffect(() => {
-        if(id){
-            db.collection('rooms').doc(id).collection('messages').orderBy('timestamp','desc').onSnapshot(snapshot => {
-                setMessages(snapshot.docs.map((doc) => doc.data()))
-            })
-        }
-    }, [id]);
+  /* show last message on sideBar */
+  useEffect(() => {
+    if (id) {
+      db.collection("rooms")
+        .doc(id)
+        .collection("messages")
+        .orderBy("timestamp", "desc")
+        .onSnapshot((snapshot) => {
+          setMessages(snapshot.docs.map((doc) => doc.data()));
+        });
+    }
+  }, [id]);
 
-    /* Random user */
-    useEffect(()=>{
-        setSeed(Math.floor(Math.random()* 5000));
-    }, []);
+  /* Random user */
+  useEffect(() => {
+    setSeed(Math.floor(Math.random() * 5000));
+  }, []);
 
-    const createChat = () => {
-        const roomName = prompt('Please enter name for chat room');
+  const createChat = () => {
+    const roomName = prompt("Please enter name for chat room");
 
-        if(roomName){
-            //push information to database
-            db.collection('rooms').add({
-                name: roomName,
-            })
-        }
-    };
+    if (roomName) {
+      //push information to database
+      db.collection("rooms").add({
+        name: roomName,
+      });
+    }
+  };
 
-    return !addNewChat ? (
-        <Link to={`/rooms/${id}`}>
-        <div className='sidebarChat'>
-            {/* Api for random user avatar */}
-            <Avatar src={`https://avatars.dicebear.com/api/human/${seed}.svg`}/>
-            <div className="sidebarChat__info">
-                <h2>{name}</h2> {/* from data base */}
-                <p>{messages[0]?.message}</p>{/* desendeing */}
-            </div>
+  return !addNewChat ? (
+    <Link to={`/rooms/${id}`}>
+      <div className="sidebarChat">
+        {/* Api for random user avatar */}
+        <Avatar src={`https://avatars.dicebear.com/api/human/${seed}.svg`} />
+        <div className="sidebarChat__info">
+          <h2>{name}</h2> {/* from data base */}
+          <p>{messages[0]?.message}</p>
+          {/* desendeing */}
         </div>
-        </Link>
-    ) : (
-        <div onClick={createChat} className='sidebarChat'>
-            <h2>Add new Chat</h2>
-        </div>
-    )
+      </div>
+    </Link>
+  ) : (
+    <div onClick={createChat} className="sidebarChat">
+      <h2>Add new Chat</h2>
+    </div>
+  );
 }
+
+SidebarChat.propTypes = {
+  addNewChat: PropTypes.any,
+  name: PropTypes.any,
+  id: PropTypes.any,
+};
 
 export default SidebarChat;
