8d65325b4b2e95b34d94a87ecb14b35c027be25a
zhpadvoiski <zh.padvoiski@gmail.com>
2021-09-09 20:55:10 -0500

    firebase bug fixed

--- src/firebase.js
+++ src/firebase.js
@@ -1,19 +1,19 @@
 //Connect database
-import firebase from 'firebase';
+import firebase from "firebase";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyDyamkO-hFkMAi2ZWAcFReDZi-0DlhMc18",
-    authDomain: "whatsapp-clone-34161.firebaseapp.com",
-    projectId: "whatsapp-clone-34161",
-    storageBucket: "whatsapp-clone-34161.appspot.com",
-    messagingSenderId: "469958437826",
-    appId: "1:469958437826:web:8162472b7617f8abf5045b",
-    measurementId: "G-2Q9HZHDBT3"
+  apiKey: "AIzaSyADWgmS7JCH3RLuECV2W-KLMwq8cfPw8bk",
+  authDomain: "whatsapp-clone-1e1a5.firebaseapp.com",
+  projectId: "whatsapp-clone-1e1a5",
+  storageBucket: "whatsapp-clone-1e1a5.appspot.com",
+  messagingSenderId: "271091523110",
+  appId: "1:271091523110:web:640d821f7fcab5503010e7",
+  measurementId: "G-FVNN3W7WCF",
 };
 
 //real time database
 const firebaseApp = firebase.initializeApp(firebaseConfig); //initialize app and store that data in this variable
-const db = firebaseApp.firestore(); //it will access the firebase instance 
+const db = firebaseApp.firestore(); //it will access the firebase instance
 const auth = firebase.auth(); //autontification
 const provider = new firebase.auth.GoogleAuthProvider(); //autontification for google
 
